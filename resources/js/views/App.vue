96514e62c7154a7a4265ef0908b129c66a0a00ff
pacimarco <m.paci1987@gmail.com>
2022-10-17 17:40:32 +0200

    error stampa navbar myheader

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
+        <MyHeader/>
         <h1>Work in progress</h1>
         <h2>Sito in costruzione</h2>
     </div>
 </template>
 
 <script>
+import MyHeader from '../components/MyHeader.vue';
 export default {
     name: "App",
-    components: {}
+    components: {
+        MyHeader
+    }
 }
 </script>
